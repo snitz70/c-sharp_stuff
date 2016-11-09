6499d1c3e31d401908ab925e457e73d07be56862
Brian Snyder <bsnyder@technology4retailers.com>
2016-11-09 12:29:20 -0600

    initial commit

---
+++ Guys/Guys/Guy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guys
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+           
+        }
+
+        public int ReceiveCash(int amount)
+        {
+
+        }
+    }
+}
