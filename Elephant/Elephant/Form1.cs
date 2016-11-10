c77b72adad0495c9d2d7220dc407a66f3f642477
Brian Snyder <bsnyder@technology4retailers.com>
2016-11-10 11:42:01 -0600

    initial commit

---
+++ Elephant/Elephant/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elephant
+{
+    public partial class Form1 : Form
+    {
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+        Elephant swap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            swap = lucinda;
+            lucinda = lloyd;
+            lloyd = swap;
+            MessageBox.Show("Objects swapped");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+    }
+}
